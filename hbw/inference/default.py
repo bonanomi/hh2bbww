fc9c201191c9f8ea9ac03e0d2fc93146f7da5470
Matteo Bonanomi <matteo9496@gmail.com>
2023-06-21 15:25:23 +0200

    [inference] Skeleton of datacard

--- hbw/inference/default.py
+++ hbw/inference/default.py
@@ -22,27 +22,36 @@ def default(self):
     e_categories, mu_categories = [], []
 
     # NOTE: use ML model inst if possible
-    ml_model_name = "default"
-    ml_model_processes = [
-        "ggHH_kl_1_kt_1_sl_hbbhww",
+    # ml_model_name = "default"
+    # ml_model_processes = [
+    #     "ggHH_kl_1_kt_1_sl_hbbhww",
+    #     "tt",
+    #     "st",
+    #     "w_lnu",
+    #     "dy_lep",
+    # ]
+
+    resonant_sel_name = "default"
+    resonant_processes = [
+        "graviton_hh_ggf_bbww_m250_madgraph",
         "tt",
         "st",
         "w_lnu",
         "dy_lep",
     ]
 
-    for proc in ml_model_processes:
+    for proc in resonant_processes:
         e_categories.append(self.add_category(
             f"cat_1e_{proc}",
-            config_category=f"1e__ml_{proc}",
-            config_variable=f"{ml_model_name}.score_{proc}_rebin1",
+            config_category=f"1e__res_{proc}",
+            config_variable=f"pt_Higggs_bb",
             mc_stats=True,
             config_data_datasets=[f"data_e_{i}" for i in ["b", "c", "d", "e", "f"]],
         ))
         mu_categories.append(self.add_category(
             f"cat_1mu_{proc}",
-            config_category=f"1mu__ml_{proc}",
-            config_variable=f"{ml_model_name}.score_{proc}_rebin1",
+            config_category=f"1mu__res_{proc}",
+            config_variable=f"pt_Higggs_bb",
             mc_stats=True,
             config_data_datasets=[f"data_mu_{i}" for i in ["b", "c", "d", "e", "f"]],
         ))
@@ -52,29 +61,20 @@ def default(self):
     #
 
     signals_ggHH = [
-        "ggHH_kl_0_kt_1_sl_hbbhww", "ggHH_kl_1_kt_1_sl_hbbhww",
-        "ggHH_kl_2p45_kt_1_sl_hbbhww", "ggHH_kl_5_kt_1_sl_hbbhww",
+        "graviton_hh_ggf_bbww_m250_madgraph"
     ]
-    signals_qqHH = [
-        "qqHH_CV_1_C2V_1_kl_1_sl_hbbhww", "qqHH_CV_1_C2V_1_kl_0_sl_hbbhww", "qqHH_CV_1_C2V_1_kl_2_sl_hbbhww",
-        "qqHH_CV_1_C2V_0_kl_1_sl_hbbhww", "qqHH_CV_1_C2V_2_kl_1_sl_hbbhww",
-        "qqHH_CV_0p5_C2V_1_kl_1_sl_hbbhww", "qqHH_CV_1p5_C2V_1_kl_1_sl_hbbhww",
-    ]
+    # signals_qqHH = [
+    #     "qqHH_CV_1_C2V_1_kl_1_sl_hbbhww", "qqHH_CV_1_C2V_1_kl_0_sl_hbbhww", "qqHH_CV_1_C2V_1_kl_2_sl_hbbhww",
+    #     "qqHH_CV_1_C2V_0_kl_1_sl_hbbhww", "qqHH_CV_1_C2V_2_kl_1_sl_hbbhww",
+    #     "qqHH_CV_0p5_C2V_1_kl_1_sl_hbbhww", "qqHH_CV_1p5_C2V_1_kl_1_sl_hbbhww",
+    # ]
 
     processes = [
-        "ggHH_kl_0_kt_1_sl_hbbhww",
-        "ggHH_kl_1_kt_1_sl_hbbhww",
-        "ggHH_kl_2p45_kt_1_sl_hbbhww",
-        "ggHH_kl_5_kt_1_sl_hbbhww",
+        "graviton_hh_ggf_bbww_m250_madgraph",
         "tt",
-        # "ttv", "ttvv",
         "st_schannel", "st_tchannel", "st_twchannel",
-        "dy_lep",
         "w_lnu",
-        # "vv",
-        # "vvv",
-        # "qcd",
-        # "ggZH", "tHq", "tHW", "ggH", "qqH", "ZH", "WH", "VH", "ttH", "bbH",
+        "dy_lep",
     ]
 
     # if process names need to be changed to fit some convention
@@ -97,7 +97,7 @@ def default(self):
         self.add_process(
             inference_procnames.get(proc, proc),
             config_process=proc,
-            is_signal=("HH_" in proc),
+            is_signal=("graviton_" in proc),
             config_mc_datasets=datasets,
         )
 
@@ -120,8 +120,6 @@ def default(self):
         "VH": ["ZH", "WH", "VH"],
         "ttH": ["ttH", "tHq", "tHW"],
         "bbH": ["bbH"],  # contains also pdf and alpha_s
-        # "ggHH": signals_ggHH,  # included in inference model (THU_HH)
-        "qqHH": signals_qqHH,
         "VHH": [],
         "ttHH": [],
     }
@@ -130,23 +128,23 @@ def default(self):
     # TODO: some scale/pdf uncertainties should be rounded to 3 digits, others to 4 digits
     # NOTE: it might be easier to just take the recommended uncertainty values from HH conventions at
     #       https://gitlab.cern.ch/hh/naming-conventions instead of taking the values from CMSDB
-    for k, procs in proc_QCDscale.items():
-        for proc in procs:
-            if proc not in processes:
-                continue
-            process_inst = self.config_inst.get_process(proc)
-            if "scale" not in process_inst.xsecs[ecm]:
-                continue
-            self.add_parameter(
-                f"QCDscale_{k}",
-                process=inference_procnames.get(proc, proc),
-                type=ParameterType.rate_gauss,
-                effect=tuple(map(
-                    lambda f: round(f, 3),
-                    process_inst.xsecs[ecm].get(names=("scale"), direction=("down", "up"), factor=True),
-                )),
-            )
-        self.add_parameter_to_group(f"QCDscale_{k}", "theory")
+    # for k, procs in proc_QCDscale.items():
+    #     for proc in procs:
+    #         if proc not in processes:
+    #             continue
+    #         process_inst = self.config_inst.get_process(proc)
+    #         if "scale" not in process_inst.xsecs[ecm]:
+    #             continue
+    #         self.add_parameter(
+    #             f"QCDscale_{k}",
+    #             process=inference_procnames.get(proc, proc),
+    #             type=ParameterType.rate_gauss,
+    #             effect=tuple(map(
+    #                 lambda f: round(f, 3),
+    #                 process_inst.xsecs[ecm].get(names=("scale"), direction=("down", "up"), factor=True),
+    #             )),
+    #         )
+    #     self.add_parameter_to_group(f"QCDscale_{k}", "theory")
 
     # add PDF rate uncertainties to inference model
     proc_pdf = {
@@ -164,24 +162,24 @@ def default(self):
         "Higgs_ttHH": ["ttHH"],
     }
 
-    for k, procs in proc_pdf.items():
-        for proc in procs:
-            if proc not in processes:
-                continue
-            process_inst = self.config_inst.get_process(proc)
-            if "pdf" not in process_inst.xsecs[ecm]:
-                continue
+    # for k, procs in proc_pdf.items():
+    #     for proc in procs:
+    #         if proc not in processes:
+    #             continue
+    #         process_inst = self.config_inst.get_process(proc)
+    #         if "pdf" not in process_inst.xsecs[ecm]:
+    #             continue
 
-            self.add_parameter(
-                f"pdf_{k}",
-                process=inference_procnames.get(proc, proc),
-                type=ParameterType.rate_gauss,
-                effect=tuple(map(
-                    lambda f: round(f, 3),
-                    process_inst.xsecs[ecm].get(names=("pdf"), direction=("down", "up"), factor=True),
-                )),
-            )
-        self.add_parameter_to_group(f"pdf_{k}", "theory")
+    #         self.add_parameter(
+    #             f"pdf_{k}",
+    #             process=inference_procnames.get(proc, proc),
+    #             type=ParameterType.rate_gauss,
+    #             effect=tuple(map(
+    #                 lambda f: round(f, 3),
+    #                 process_inst.xsecs[ecm].get(names=("pdf"), direction=("down", "up"), factor=True),
+    #             )),
+    #         )
+    #     self.add_parameter_to_group(f"pdf_{k}", "theory")
 
     # lumi
     lumi = self.config_inst.x.luminosity
@@ -204,21 +202,21 @@ def default(self):
     """
 
     # scale + pdf (shape)
-    for proc in processes:
-        if proc == "qcd":
-            # no scale/pdf shape uncert. for qcd
-            continue
-        for unc in ("murf_envelope", "pdf"):
-            if proc == "st_tchannel" and unc == "pdf":
-                # TODO: debugging (unphysically large/small pdf weights in process)
-                continue
-            self.add_parameter(
-                f"{unc}_{proc}",
-                process=inference_procnames.get(proc, proc),
-                type=ParameterType.shape,
-                config_shift_source=f"{unc}",
-            )
-            self.add_parameter_to_group(f"{unc}_{proc}", "theory")
+    # for proc in processes:
+    #     if proc == "qcd":
+    #         # no scale/pdf shape uncert. for qcd
+    #         continue
+    #     for unc in ("murf_envelope", "pdf"):
+    #         if proc == "st_tchannel" and unc == "pdf":
+    #             # TODO: debugging (unphysically large/small pdf weights in process)
+    #             continue
+    #         self.add_parameter(
+    #             f"{unc}_{proc}",
+    #             process=inference_procnames.get(proc, proc),
+    #             type=ParameterType.shape,
+    #             config_shift_source=f"{unc}",
+    #         )
+    #         self.add_parameter_to_group(f"{unc}_{proc}", "theory")
 
     #
     # post-processing
